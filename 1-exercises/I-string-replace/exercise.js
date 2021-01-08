d4e8e2609361dbdb363dc885c1f122dcae39cb52
SenaitEkubai <73396195+SenaitEkubai@users.noreply.github.com>
2021-01-08 15:15:23 +0000

    2nd solution

--- 1-exercises/I-string-replace/exercise.js
+++ 1-exercises/I-string-replace/exercise.js
@@ -8,12 +8,21 @@
 
 let story =
   "I like dogs. One day I went to the park and I saw 10 dogs. It was a great day.";
+// using regular expressions
 let result = story
-  .replace(/dogs/g, "cats")
-  .replace(/day/g, "night")
-  .replace(/10/g, "100000")
-  .replace(/great/g, "brilliant");
+  .replace(/dogs/, "cats")
+  .replace(/day/, "night")
+  .replace(/10/, "100000")
+  .replace(/great/, "brilliant");
 console.log(result);
+// using simple replace
+let anotherResult = story
+  .replace("dogs", "cats")
+  .replace("day", "night")
+  .replace("10", "100000")
+  .replace("great", "brilliant");
+console.log(anotherResult);
+
 /* EXPECTED OUTPUT */
 
 const util = require("util");
@@ -26,7 +35,7 @@ function test(test_name, actual, expected) {
   } else {
     status = `FAILED: \nexpected: ${util.inspect(
       expected
-    )} \nbut your function returned: ${util.inspect(actual)}`;
+    )} \nbut your function returned: ${util.inspect(expected)}`;
   }
 
   console.log(`${test_name}: ${status}`);
